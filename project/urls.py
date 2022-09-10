d6716fac8627ad14ffe26b94f6ba9f8db41b0800
Afshar Sharifi <afsharsharifi2020@gmail.com>
2022-09-11 02:51:47 +0430

    Project app CRUD for now

---
+++ project/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("create", views.ProjectCreateView.as_view()),
+    path("read/<int:pk>", views.ProjectReadView.as_view()),
+    path("update/<int:pk>", views.ProjectUpdateView.as_view()),
+    path("delete/<int:pk>", views.ProjectDeleteView.as_view()),
+]
--- project/views.py
+++ project/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from .models import Project
+from .serializers import ProjectSerializer
+from rest_framework import generics
 # Create your views here.
+
+
+class ProjectCreateView(generics.CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectReadView(generics.RetrieveAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectUpdateView(generics.UpdateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectDeleteView(generics.DestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
