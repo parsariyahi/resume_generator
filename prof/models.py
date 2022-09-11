7f2ae59a2c6f912459a9aba045de9b4842dca244
parsariyahi <pany.parsariyahi@gmail.com>
2022-09-11 11:09:12 +0430

    added nullable for fields in models

--- prof/models.py
+++ prof/models.py
@@ -4,8 +4,24 @@ from user.models import User
 
 
 class Prof(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_profile")
-    title = models.CharField(max_length=150)
-    job_position = models.CharField(max_length=150)
-    bio = models.TextField()
-    profile_img = models.ImageField()
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="user_profile"
+    )
+    title = models.CharField(
+        max_length=150, default=None,
+        null=True, blank=True,
+    )
+    job_position = models.CharField(
+        max_length=150, default=None,
+        null=True, blank=True,
+    )
+    bio = models.TextField(
+        default=None, null=True, blank=True,
+    )
+    profile_img = models.ImageField(
+        default=None, null=True, blank=True,
+    )
+
+    def __str__(self) :
+        return self.user.username
