72b3fec11d278b6a4acafb7983d1535908adf740
Afshar Sharifi <afsharsharifi2020@gmail.com>
2022-09-12 12:52:25 +0430

    adding / in URLs skill

--- skill/urls.py
+++ skill/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("create/", views.SkillCreateView.as_view()),
     path("read/<int:pk>/", views.SkillReadView.as_view()),
-    path("update/<int:pk>", views.SkillUpdateView.as_view()),
-    path("delete/<int:pk>", views.SkillDeleteView.as_view()),
+    path("update/<int:pk>/", views.SkillUpdateView.as_view()),
+    path("delete/<int:pk>/", views.SkillDeleteView.as_view()),
 ]
